1bda3d88be763b34ced2d9c2cea3a98507de24d9
Anthony Cecconato <antho.cecc@gmail.com>
2021-06-05 01:24:39 +0200

    Modify some regex

--- assets/js/services/Validator.js
+++ assets/js/services/Validator.js
@@ -4,34 +4,43 @@
  * @return {boolean}
  */
 export function isValidEmail(str) {
-    const regex = new RegExp(/^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/);
+    const regex = new RegExp(/^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/i);
     return !!str.match(regex);
 }
 
 /**
+ * Should start nor end with a letter, a digit, é or è
+ * Can contains Letters, digits, é, è, comma
+ * Can be separated by one whitespace, apostrophe, hyphen, underscore
  * @param str
  * @return {boolean}
  */
 export function isValidAddress(str) {
-    const regex = new RegExp(/^[a-z\d]+(?:[\w\d,\-\_\'\"]+\s)*[a-z\d]+$/);
+    const regex = new RegExp(/^(?:(?:[\w\déè]+[,]*[\s|\'|\-|\_])*)[a-zA-Zéè\d]+$/i);
     return !!str.match(regex);
 }
 
 /**
+ * Should start nor end with a letter, é or è
+ * Can contains Letters, é, è
+ * Can be separated by one whitespace, apostrophe, hyphen
  * @param str
  * @return {boolean}
  */
 export function isValidName(str) {
-    const regex = new RegExp(/^[a-z]+[\w -]+[a-z]+$/i);
+    const regex = new RegExp(/^(?:(?:[\wéè]+[\s|\'|\-])*)[a-zA-Zéè]+$/i);
     return !!str.match(regex);
 }
 
 /**
+ * Should start nor end with a letter, a digit, é or è
+ * Can contains Letters, digits, é, è
+ * Can be separated by one whitespace, apostrophe, hyphen, underscore
  * @param str
  * @return {boolean}
  */
 export function isValidCity(str) {
-    const regex = new RegExp(/^[a-z]+[\w \-\_\'\"]+[a-z]+$/i);
+    const regex = new RegExp(/^(?:(?:[\w\déè]+[\s|\'|\-|\_])*)[a-zA-Zéè]+$/i);
     return !!str.match(regex);
 }
 
