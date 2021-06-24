6249e31fda651e7947e72396ec28c0e3f776f916
Anthony Cecconato <antho.cecc@gmail.com>
2021-06-24 11:54:17 +0200

    Change API url and remove lorem ipsum from the homepage

--- assets/js/config/config.js
+++ assets/js/config/config.js
@@ -1,6 +1,6 @@
 export default {
   basePath: 'localhost:8080',
-  apiUrl: 'http://localhost:3000/api',
+  apiUrl: 'http://jwdp5.anthony-cecconato.fr/api',
 
   types_options: { teddies: 'colors', cameras: 'lenses', furniture: 'varnish' },
 };
